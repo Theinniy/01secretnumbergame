8f47798032d4ff0a379f260fc50fe08c0f0f24e4
Theinniy <thiy.amorim@gmail.com>
2025-03-09 18:44:34 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados =[];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,8 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    let mensagemInicial = `Escolha um número entre 1 e ${numeroLimite}`;
+    exibirTextoNaTela('p', mensagemInicial);
 }
 
 exibirMensagemInicial();
